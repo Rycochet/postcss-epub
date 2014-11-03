655656f6fbbf295c9355bac547c7eb50af6a0170
Robin Cloutman <rycochet@rycochet.com>
2014-11-03 10:52:20 +0000

    Fix so it works in grunt etc

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
+"use strict";
+
 /**
  * Properties to prefix.
  */
-var props = [
-	"hyphens",
-	"line-break",
-	"text-align-last",
-	"text-emphasis",
-	"text-emphasis-color",
-	"text-emphasis-style",
-	"word-break"
-];
+var postcss = require("postcss"),
+		props = [
+			"hyphens",
+			"line-break",
+			"text-align-last",
+			"text-emphasis",
+			"text-emphasis-color",
+			"text-emphasis-style",
+			"word-break"
+		];
 
 /**
  * PostCSS plugin to prefix ePub3 properties.
+ * @param {Object} style
  */
-module.exports = function plugin() {
-	return function(style) {
-		style.eachDecl(function(decl) {
-			if (decl.value) {
-				try {
-					if (props.indexOf(decl.prop) >= 0) {
-						decl.insertBefore(decl.clone({prop: "-epub-" + decl.prop}));
-					}
-				} catch (err) {
-					err.message = gnuMessage(err.message, decl.source);
-					throw err;
+function plugin(style) {
+	style.eachDecl(function(decl) {
+		if (decl.value) {
+			try {
+				if (props.indexOf(decl.prop) >= 0) {
+					decl.parent.insertBefore(decl, decl.clone({prop: "-epub-" + decl.prop}));
 				}
+			} catch (err) {
+				err.message = gnuMessage(err.message, decl.source);
+				throw err;
 			}
-		});
-	};
-};
+		}
+	});
+}
 
 /**
  * return GNU style message
@@ -40,3 +42,13 @@ module.exports = function plugin() {
 function gnuMessage(message, source) {
 	return (source ? (source.file ? source.file : "<css input>") + ":" + source.start.line + ":" + source.start.column : "") + " " + message;
 }
+
+/*
+ * ...and export for use...
+ */
+module.exports = {
+	postcss: plugin,
+	process: function(css, opts) {
+		return postcss(plugin).process(css, opts).css;
+	}
+};
